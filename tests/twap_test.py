8433cdc48d6466963cdb5ca6ca65536d75a43107
Hichemchir <hichem.C@outlook.com>
2026-01-09 19:36:19 +0100

    typo

--- tests/twap_test.py
+++ tests/twap_test.py
@@ -6,7 +6,7 @@ df = load_data(DATA_PATH)
 # AAA: Arrange (setup), Act (execute func), Assert (verify)
 class TestTWAP:
     def test_executes_all_slices(self) -> None:
-        order = Order(size=10000, direction="BUY", num_slices=5)
+        order = Order(size=10000, direction="buy", num_slices=5)
         result = execute_twap(df, order, start_idx=0)
 
         assert len(result.slices) == order.num_slices, "Should execute 5 slices"
